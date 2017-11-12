3e859c17ed14fcab5068e2ca1c4c1507b0d1c733
Jacky Zhao <liang.zhao83@gmail.com>
2017-11-11 21:11:17 -0800

    working version

--- services/parser.js
+++ services/parser.js
@@ -22,26 +22,26 @@ module.exports = {
   },
 
   /*
-    parse product name from Detail page
+    parse product info from Detail page
   */
   parseProductINFO: (rawHTML, url) => {
-
-    // const header = ['NAME', 'ASIN', 'ABSR', 'PRICE', 'REVIEW', 'DIMENSION', 'WEIGHT', 'URL'];
-
 
     logger.log('parse productINFO for URL', url);
 
     let productINFO = {
-      NAME: '',
-      ASIN: '',
-      ABSR: '',
-      PRICE: 0,
-      STAR: 0,
-      REVIEW: 0,
-      DIMENSION: '',
-      WEIGHT: 0,
-      UNIT: '',
-      URL: ''
+      NAME: 'n/a',
+      ASIN: 'n/a',
+      ABSR: 'n/a',
+      PRICE: 'n/a',
+      STAR: 'n/a',
+      REVIEW: 'n/a',
+      DIMENSION: 'n/a',
+      LENGTH: 'n/a',
+      WIDTH: 'n/a',
+      HEIGHT: 'n/a',
+      WEIGHT: 'n/a',
+      UNIT: 'n/a',
+      URL: 'n/a'
     };
     const $ = cheerio.load(rawHTML);
 
@@ -49,11 +49,8 @@ module.exports = {
     name = _.trim(name.html());
 
     let price = $('#priceblock_ourprice').text();
-    // console.log('priceblock_ourprice ', price);
-    // console.log('typeof price ', typeof price);
     if (typeof price == 'undefined' || price == '') {
       price = $('#priceblock_saleprice').text();
-      // console.log('priceblock_saleprice ', price);
     }
     price = price.split('$')[1];
 
@@ -61,41 +58,48 @@ module.exports = {
     productINFO.URL = url;
     productINFO.PRICE = price;
 
-    // item  ProductDimensions:7.5x2.5x6.8inches;1.1pounds
-    // item  ShippingWeight:1.2pounds(Viewshippingratesandpolicies)
-    // item  DomesticShipping:Currently,itemcanbeshippedonlywithintheU.S.andtoAPO/FPOaddresses.ForAPO/FPOshipments,pleasecheckwiththemanufacturerregardingwarrantyandsupportissues.
-    // item  InternationalShipping:Thisitemisnoteligibleforinternationalshipping.LearnMore
-    // item  ASIN:B00CYBXKR8
-    // item  UPC:813859021153020424101114
-    // item  Itemmodelnumber:10HBC01111
-    // item  AverageCustomerReview:4.4outof5stars143customerreviews
-    // item  AmazonBestSellersRank:#1,104inHealth&Household(SeeTop100inHealth&Household)#2inSports&Outdoors>OutdoorRecreation>Camping&Hiking>Safety&Survival>FirstAidKits#3inHealth&Household>HealthCare>FirstAid>FirstAidKits
-
-
     $('#detail-bullets > table > tbody > tr > td > div.content > ul > li').each(function(i, elem) {
       let item = $(this).text();
       item = item.replace(/\r?\n|\r/g, '');
       item = item.replace(/\s/g, '');
-      console.log('item ', item);
 
       if (item.indexOf('ASIN') != -1) {
-        console.log('ASIN...');
         item = item.split(':')[1];
         productINFO.ASIN = item;
       }
       if (item.indexOf('AmazonBestSellersRank') != -1) {
-        console.log('AmazonBestSellersRank...');
         item = item.split(':#')[1];
         item = item.split('in')[0];
         productINFO.ABSR = item;
       }
       if (item.indexOf('AverageCustomerReview') != -1) {
-        console.log('AverageCustomerReview...');
         item = item.split(':')[1];
         let star = item.split('outof')[0];
         let review = item.split('stars')[1].split('customerreviews')[0];
         productINFO.STAR = star;
         productINFO.REVIEW = review;
+      }
+      if (item.indexOf('ProductDimensions') != -1) {
+        item = item.split(':')[1].split('inches')[0];
+        let dimension = item;
+        let dimensions = item.split('x');
+        productINFO.DIMENSION = dimension + ' inches';
+        productINFO.LENGTH = dimensions[0];
+        productINFO.WIDTH = dimensions[1];
+        productINFO.HEIGHT = dimensions[2];
+      }
+      if (item.indexOf('ShippingWeight') != -1) {
+        item = item.split(':')[1];
+        let weight, unit;
+        if (item.indexOf('pounds') != -1) {
+          unit = 'pounds';
+          weight = item.split('pounds')[0];
+        } else {
+          unit = 'ounces';
+          weight = item.split('ounces')[0];
+        }
+        productINFO.WEIGHT = weight;
+        productINFO.UNIT = unit;
       }
 
     });
